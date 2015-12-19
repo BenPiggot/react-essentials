eb4098f7e5d1d7a79f12e3c862310f09c76acad2
wbp <wbpiggot@gmail.com>
2015-12-18 23:20:29 -0800

    December 18 update

--- source/components/__tests__/Header-test.js
+++ source/components/__tests__/Header-test.js
@@ -1,28 +1,24 @@
 jest.dontMock('../Header.react');
 
-describe('Header component', function() {
-
-  it('renders provided header content', function() {
-
+describe('Header component', function () {
+  it('renders provided header text', function () {
     var React = require('react');
     var ReactDOM = require('react-dom');
     var TestUtils = require('react-addons-test-utils');
     var Header = require('../Header.react');
 
     var header = TestUtils.renderIntoDocument(
-      <Header text='testing...' />
+      <Header text="Testing..." />
     );
+    var actualHeaderText = ReactDOM.findDOMNode(header).textContent;
 
-    var actualHeaderText = ReactDOM.findDOMNode(header).textContext;
-
-    expect(actualHeaderText).toEqual('testing...');
-
-    var defaultHeader.renderIntoDocument(
+    expect(actualHeaderText).toBe('Testing...');
+    
+    var defaultHeader = TestUtils.renderIntoDocument(
       <Header />
     );
-
-    var actualDefaultHeaderText = ReactDOM.findDOMNode(header).textContext;
-
-    expect(actualDefaultHeaderText).toEqual('Default header');
-  })
-})
+    var actualDefaultHeaderText = ReactDOM.findDOMNode(defaultHeader).textContent;
+    
+    expect(actualDefaultHeaderText).toBe('Default header');
+  });
+});
